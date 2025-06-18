fc2b8151b45ad476c7af32d97c134f2fb680e516
Akshaya Singh <akshaysing975@gmail.com>
2025-06-18 19:17:58 +0530

    update navbar

--- tasks/component/Navbar.js
+++ tasks/component/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-xl font-bold">
-          Task Manager -Docker
+          Task Manager
         </Link>
         <div className="space-x-4">
           <Link href="/dashboard" className="hover:text-gray-400">
